2ef560bc9a7298099d33ead0deec2923b69f4317
Sam Collins <theatrepro@hotmail.co.uk>
2015-11-11 21:43:05 +0000

    Logout Route

--- src/SCollins/SteamAuth/Http/routes.php
+++ src/SCollins/SteamAuth/Http/routes.php
@@ -1,2 +1,3 @@
 <?php
 Route::get('/'.config('steam.loginUrl', 'steamlogin'), 'SCollins\SteamAuth\Http\SteamController@redirect');
+Route::get('steamlogout', 'SCollins\SteamAuth\Http\SteamController@logout');
